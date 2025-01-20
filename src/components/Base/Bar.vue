725434cd5a3e493c3251574bc41767759bdcf225
Dennis Adriaansen <dennis.a@socialdeal.nl>
2025-01-20 11:11:36 +0100

    Update bar chart

--- src/components/Base/Bar.vue
+++ src/components/Base/Bar.vue
@@ -3,52 +3,60 @@ import { VisAxis, VisBulletLegend, VisGroupedBar, VisTooltip, VisXYContainer } f
 
 const props = defineProps<{
   data: T[]
+  height: number,
   categories: {
     name: string
     color: string
   }[]
-  xAxis: (d: T) => T[keyof T]
   yAxis: Array<(d: T) => T[keyof T]>
   xLabel: string
   yLabel: string
-  xFormatter: (v: string) => string
+  xFormatter: (index: number) => string
   xTickCount: number
   barPadding?: number
   tooltipTriggers: any
 }>()
 
-const color = (_: T, i: number) => props.categories[i].color
+const color = (d: T, i: number) => props.categories[i].color
 </script>
 
 <template>
-  <VisBulletLegend
-    :items="categories"
-  />
-  <VisXYContainer
-    :height="500"
-  >
-    <VisTooltip :triggers="tooltipTriggers" />
-    <VisGroupedBar
-      :data="data"
-      :x="xAxis"
-      :y="yAxis"
-      :color="color"
-      :bar-padding="barPadding ?? 0"
-    />
-    <VisAxis
-      type="x"
-      :label="xLabel"
-      :grid-line="false"
-      :domain-line="false"
-      :num-ticks="xTickCount ?? data.length"
-      :tick-format="xFormatter"
-    />
-    <VisAxis
-      type="y"
-      :grid-line="true"
-      :domain-line="false"
-      :tick-format="(value: number) => (value / 10 ** 6).toFixed(1)"
-      :label="yLabel"
-    />
-  </VisXYContainer>
+  <div class="space-y-2">
+    <VisXYContainer
+      :height="height"
+    >
+      <VisTooltip :triggers="tooltipTriggers" />
+      <VisGroupedBar
+        :data="data"
+        :x="(_: T, i: number) => i"
+        :y="yAxis"
+        :color="color"
+        :group-padding="0.5"
+        :bar-padding="barPadding ?? 0"
+        :rounded-corners="40"
+        :grid-line="false"
+        :domain-line="false"
+      />
+      <VisAxis
+        type="x"
+        :label="xLabel"
+        :grid-line="false"
+        :domain-line="false"
+        :num-ticks="xTickCount ?? data.length"
+        :tick-format="xFormatter"
+      />
+      <VisAxis
+        type="y"
+        :grid-line="true"
+        :domain-line="false"
+        :tick-format="(value: number) => (value / 10 ** 6).toFixed(1)"
+        :label="yLabel"
+      />
+    </VisXYContainer>
+    <div class="flex items center justify-end">
+      <VisBulletLegend
+        :items="categories"
+      />
+    </div>
+  </div>
 </template>
